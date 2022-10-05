8556b6dd85bf86537a5b709e6833fbf6d431d2df
Iara Gimenez-Antoniow <iaragimeneza@gmail.com>
2022-10-05 11:10:23 -0300

    cambio info que recibe card- Ahora es un objeto

--- src/component/CardComponent.jsx
+++ src/component/CardComponent.jsx
@@ -1,12 +1,12 @@
-const CardComponent = ({title, img, agregarAlCarrito}) => {
+const CardComponent = ({product, agregarAlCarrito}) => {
     return (
         <div className="card col-4">
-            <img className="card-img-top" src={img} alt="Card image cap"/>
+            <img className="card-img-top" src={product.img} alt="Card image cap"/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">Some quick example text</p>
                 <button className="btn btn-primary" onClick={(event) => 
-                    {agregarAlCarrito(event,title)}}
+                    {agregarAlCarrito(event,product)}}
                     >Agregar al carrito</button>
             </div>
         </div> 
