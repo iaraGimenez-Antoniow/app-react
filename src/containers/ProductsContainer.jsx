f80b55d001427a7b57459b5c2bdfafa85c905646
Iara Gimenez-Antoniow <iaragimeneza@gmail.com>
2022-10-05 16:51:10 -0300

    uso de API

--- src/containers/ProductsContainer.jsx
+++ src/containers/ProductsContainer.jsx
@@ -3,24 +3,15 @@ import CardComponent from "../component/CardComponent";
 import InfoBarComponent from "../component/InfoBar";
 
 const ProductsContainer = () => {
-    const [carrito, setCarrito] = useState([])
+    const [carrito, setCarrito] = useState([]);
     const [products, setProducts] = useState([]);
 
 
-    useEffect(() => {
-        //cuando se esta por montar un component
-        //llamada a una API
-        const data = [
-            {id:1,name:'shoes',
-            img:'https://cdn.soyunperro.com/wp-content/uploads/perrocontento.jpg'},
-            {id:2,name:'shirt',
-            img:'https://cdn.soyunperro.com/wp-content/uploads/perrocontento.jpg'},
-            {id:3,name:'books',
-            img:'https://cdn.soyunperro.com/wp-content/uploads/perrocontento.jpg'},
-        ]
-        setProducts(data);
+    useEffect(async () => {
+        let data = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=mac');
+        let response = await data.json();
+        setProducts(response.results);
         return () => {
-            //cuando se esta por morir un component
             console.log('Se esta por morir el comente');
         }
     }, []);    
@@ -29,14 +20,15 @@ const ProductsContainer = () => {
         setCarrito([...carrito]);
         console.log(carrito);
     }
-    console.log(carrito);
     return (
         <div className="container bg-warning">
             <InfoBarComponent carrito={carrito} />
             <div className="row px-2 py-2">
-                {products.map(element => {
+                {products.map((element, index) => {
                     return (
-                        <CardComponent product={element} agregarAlCarrito={AgregarAlCarrito }/>
+                        <span key={index}>
+                            <CardComponent product={element} agregarAlCarrito={AgregarAlCarrito }/>
+                        </span>
                     )
                 })}
             </div>
